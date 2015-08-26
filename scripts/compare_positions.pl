d6c750cde3c5cdc5ff08fd6644af4ba788cfffbf
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-08-26 15:06:07 -0500

    Updated terms in compare_positions to use true positive, ...

--- scripts/compare_positions.pl
+++ scripts/compare_positions.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 # Purpose
-# Given two positions tables of variants, determines the number of unique and non-unique positions
+# Compares two position tables of variants with respect to TP/FP/TN/FN.
 
 use strict;
 use warnings;
@@ -8,6 +8,12 @@ use warnings;
 use Set::Scalar;
 
 # read positions file a Set::Scalar object
+# format
+# {
+#	'header' => header_line,
+# 	'positions-valid' => Set of position lines with 'valid' status
+#	'positions-invalid' => Set of position lines with 'invalid' status
+# }
 sub read_pos
 {
 	my ($file) = @_;
@@ -19,48 +25,53 @@ sub read_pos
 	chomp($header);
 	die "Error with header line: $header\n" if ($header !~ /^#/);
 	$results{'header'} = $header;
-	$results{'positions'} = Set::Scalar->new;
+	$results{'positions-valid'} = Set::Scalar->new;
+	$results{'positions-invalid'} = Set::Scalar->new;
 	while(my $line = readline($fh))
 	{
 		chomp($line);
 		my @tokens = split(/\t/,$line);
 
-		my ($chrom,$position,$status,$reference) = @tokens;
-		die "Error with line $line, position invalid\n" if (not defined $position or $position !~ /\d+/);
+		my ($chrom,$position,$status,@bases) = @tokens;
+		die "Error with line $line in $file\n" if (not defined $chrom or not defined $position or $position !~ /\d+/);
+		die "Error with line $line in $file, status not properly defined\n" if (not defined $status or $status eq '');
 
-		# ignore anything not valid
-		next if ($status ne 'valid');
-
-		$results{'positions'}->insert($line);
+		my $line_minus_status = join(' ',$chrom,$position,@bases);
+		if ($status eq 'valid') {
+			$results{'positions-valid'}->insert($line_minus_status);
+		} else {
+			$results{'positions-invalid'}->insert($line_minus_status);
+		}
 	}
 	close($fh);
 
 	return \%results;
 }
 
-my $usage = "$0 [variants1.tsv] [variants2.tsv]\n";
+my $usage = "$0 [variants-true.tsv] [variants-detected.tsv]\n";
 
 die "error:\n$usage" if (@ARGV != 2);
 
-my ($variants1_f,$variants2_f) = @ARGV;
+my ($variants_true_file,$variants_detected_file) = @ARGV;
 
-my $variants1 = read_pos($variants1_f);
-my $variants2 = read_pos($variants2_f);
+my $variants_true = read_pos($variants_true_file);
+my $variants_detected = read_pos($variants_detected_file);
 
 # must have same genomes and same order of genomes
-if ($variants1->{'header'} ne $variants2->{'header'})
+if ($variants_true->{'header'} ne $variants_detected->{'header'})
 {
 	die "Error: headers did not match\n";
 }
 
-my $variants1_positions = $variants1->{'positions'};
-my $variants2_positions = $variants2->{'positions'};
+my $var_true_valid_pos = $variants_true->{'positions-valid'};
+my $var_detected_valid_pos = $variants_detected->{'positions-valid'};
 
 # set operations
-my $intersection = $variants1_positions * $variants2_positions;
-my $uniq_variants1 = $variants1_positions - $variants2_positions;
-my $uniq_variants2 = $variants2_positions - $variants1_positions;
+my $true_positives = $var_true_valid_pos * $var_detected_valid_pos;
+my $false_positives = $var_detected_valid_pos - $var_true_valid_pos;
+my $true_negatives = Set::Scalar->new;
+my $false_negatives = $var_true_valid_pos - $var_detected_valid_pos;
 
-print "$variants1_f\t$variants2_f\tIntersection\tUnique-$variants1_f\tUnique-$variants2_f\n";
-print $variants1_positions->size,"\t",$variants2_positions->size,"\t",$intersection->size,
-	"\t",$uniq_variants1->size,"\t",$uniq_variants2->size,"\n";
+print "$variants_true_file\t$variants_detected_file\tTP\tFP\tTN\tFN\n";
+print $var_true_valid_pos->size,"\t",$var_detected_valid_pos->size,"\t",$true_positives->size,
+	"\t",$false_positives->size,"\t",$true_negatives->size,"\t",$false_negatives->size,"\n";
